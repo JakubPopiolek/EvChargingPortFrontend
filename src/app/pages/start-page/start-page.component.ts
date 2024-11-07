2c86342ca3288f50d8822e37369c4fb0eb111ea5
Jakub <jakub.popiolek@informed.com>
2024-11-07 16:58:29 +0000

    error page when service fails before submission

--- src/app/pages/start-page/start-page.component.ts
+++ src/app/pages/start-page/start-page.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { SERVICE_NAME } from '../../consts/config';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { ApiSubmitApplicationService } from '../../core/services/api/submit-application-service';
-import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-start-page',
   providers: [],
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './start-page.component.html',
   styleUrl: './start-page.component.scss',
 })
