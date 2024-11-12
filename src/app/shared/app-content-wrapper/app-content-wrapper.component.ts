0d44ee58b5c3a4dac0f64f138857f71c729e0758
Jakub <jakub.popiolek@informed.com>
2024-11-12 11:55:58 +0000

    removing comment and unused import

--- src/app/shared/app-content-wrapper/app-content-wrapper.component.ts
+++ src/app/shared/app-content-wrapper/app-content-wrapper.component.ts
@@ -3,7 +3,6 @@ import { StartPageComponent } from '../../pages/start-page/start-page.component'
 
 @Component({
   standalone: true,
-  imports: [StartPageComponent],
   selector: 'app-content-wrapper',
   templateUrl: './app-content-wrapper.component.html',
   styleUrl: './app-content-wrapper.component.scss',
